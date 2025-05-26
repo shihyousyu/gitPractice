536383c01c2f958da7663d9689b6b0bf928147ab
Timleeeeeeee59 <52fu6m4@gmail.com>
2025-05-26 20:10:45 +0800

    aaa.cpp

--- aaa.cpp
+++ aaa.cpp
@@ -1,21 +1,12 @@
 #include<iostream>
 using namespace std;
 
-void syu(){
-	cout << "hello, syu.\n";
-}
-
-void imyantsss(void) {
-	cout << "yantsssssssss" << endl;
-};
-
 void sheep(void){
 	cout<< "sheep" << endl;
 }
 
 int main(){
 	cout << "hello, world";
-  syu();
-	imyantsss();
+	sheep();
 	return 0;
 }
